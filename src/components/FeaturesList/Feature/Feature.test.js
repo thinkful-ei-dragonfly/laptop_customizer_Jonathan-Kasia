d0c5dd43c378e4054f6478867f577b152f6e0d67
Jonathan Moore <jmjonemoore@gmail.com>
2019-05-26 20:17:58 -0500

    Working on Features Test

--- src/components/FeaturesList/Feature/Feature.test.js
+++ src/components/FeaturesList/Feature/Feature.test.js
@@ -2,22 +2,59 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import renderer from 'react-test-renderer'
 import Feature from './Feature'
-// import {shallow} from 'enzyme'
-// import toJson from 'enzyme-to-json'
-// import { isMainThread } from 'worker_threads';
+import { shallow } from 'enzyme'
+import toJson from 'enzyme-to-json'
+
+this.state = {
+  selected: {
+    Display: {
+        name: '15.6" UHD (3840 x 2160) 60Hz Bright Lights and Knobs',
+        cost: 1500
+      }
+  }
+}
+
+const updateFeature = (feature, newValue) => {
+  const selected = Object.assign({}, this.state.selected);
+  selected[feature] = newValue;
+  this.setState({
+    selected
+  });
+}
+
+const item = {
+  name: '15.3" HGTV (3840 x 2160) Home makeover edition',
+  cost: 1400
+}
 
 describe('Feature Component', () => {
   it("Renders without crashing", () =>{
     const div = document.createElement('div')
-    ReactDOM.render(<Feature />, div)
+    ReactDOM.render(<Feature featureHeader='Display' item={item} featureClass={`feature__option ''`} updateFeature={updateFeature} />, div)
     ReactDOM.unmountComponentAtNode(div)
   })
 
   it("Renders the UI as expected", () =>{
     const tree = renderer
-    .create(<Feature />)
+    .create(<Feature featureHeader='Display' item={item} featureClass={`feature__option`} updateFeature={updateFeature} />)
     .toJSON();
-    expect(tree).toMatchSnapshot();
+    expect(tree).toMatchSnapshot()
+  })
+
+  it("Renders UI as expected when feature is selected", () => {
+    const wrapper = shallow(<Feature featureHeader='Display' item={item} featureClass={`feature__option feature__selected`} updateFeature={updateFeature} />)
+    expect(toJson(wrapper)).toMatchSnapshot()
+  })
+
+  it("Renders UI as expected when feature is not selected", () => {
+    const wrapper = shallow(<Feature featureHeader='Display' item={item} featureClass={`feature__option`} updateFeature={updateFeature} />)
+    expect(toJson(wrapper)).toMatchSnapshot()
+  })
+
+  it("Changes feature to 'selected' when selected", () => {
+    const wrapper = shallow(<Feature featureHeader='Display' item={item} featureClass={`feature__option`} updateFeature={updateFeature} />)
+    wrapper.find("div").simulate('click')
+    expect(toJson(wrapper)).toMatchSnapshot()
   })
 
 })
